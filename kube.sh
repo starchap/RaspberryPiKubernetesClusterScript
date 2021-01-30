21dfeba7d69cbd1e1434118f37044a29568e617b
Mikkel <mbrbayard@live.dk>
2021-01-30 16:10:20 +0100

    123

--- kube.sh
+++ kube.sh
@@ -69,7 +69,7 @@ then
 	then
 		echo "Configuring node public key"
 		sudo mkdir ./.ssh
-		echo $sshpub >> ./.ssh/authorized_keys
+		sudo bash -c 'echo "$sshpub" > ./.ssh/authorized_keys'
 		sudo chmod 600 ./.ssh/authorized_keys
 		sudo chmod 700 .shh
 		echo "The public ssh key is now in use"
